40769c88c164d9bbe8962601049f10cb89873159
Tazida <tazidarahman5@gmail.com>
2022-08-05 01:38:06 +0600

    added multi stage

---
+++ multi_stage_condition.js
@@ -0,0 +1,14 @@
+var money = 30;
+var danishPrice = 45;
+var butterBread = 35;
+var toastBiscuit = 20;
+
+if (danishPrice < money) {
+  console.log("den den danish khabo");
+} else if (butterBread < money) {
+  console.log("butter bon khabo");
+} else if (toastBiscuit < money) {
+  console.log("chubai chubai toast biscuit khabo");
+} else {
+  console.log("khali cha e bhalo");
+}
--- multiple_condition.js
+++ multiple_condition.js
@@ -12,11 +12,21 @@
 // } else {
 //   console.log("ekhon jawar iccha nai");
 // }
-var isGraduated = true;
-var salary = 75000;
-var car = 1;
-if (isGraduated == true && salary > 50000 && car >= 1) {
-  console.log("cholo spain chole jai");
-} else {
-  console.log("ekhon jawar iccha nai");
+// var isGraduated = true;
+// var salary = 75000;
+// var car = 1;
+// if (isGraduated == true && salary > 50000 && car >= 1) {
+//   console.log("cholo spain chole jai");
+// } else {
+//   console.log("ekhon jawar iccha nai");
+// }
+var isGraduated = false;
+var salary = 51000;
+var car = 0;
+
+if (isGraduated === true || salary > 50000 || car >= 1) {
+  console.log("eibar turkey jabo");
+}
+
+if (isGraduated === true && (salary > 50000 || car >= 1)) {
 }
