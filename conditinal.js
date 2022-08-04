d3aeaaf102a7d9330be9764a0d8df4b5e56470a7
Tazida <tazidarahman5@gmail.com>
2022-08-05 01:20:05 +0600

    added condition

---
+++ conditinal.js
@@ -0,0 +1,16 @@
+var iphonePrice = 79000;
+var myBudget = 950000;
+// if iphone price is less than my budget. I will buy the iphone
+/**
+ * if iphone price is less than my budget
+ *      i will buy the iphone
+ *
+ *
+ * if (condition) {
+ *        / will execute if the condition is true
+ * }
+ *
+ **/
+if (iphonePrice < myBudget) {
+  console.log("iphone kine fulani marbo");
+}
---
+++ multiple_condition.js
@@ -0,0 +1,22 @@
+// var isGraduated = true;
+// var salary = 25000;
+// if (isGraduated === true) {
+//   console.log("cholo spain chole jai");
+// } else {
+//   console.log("ekhon jawar iccha nai");
+// }
+// var isGraduated = true;
+// var salary = 25000;
+// if (isGraduated === true && salary > 50000) {
+//   console.log("cholo spain chole jai");
+// } else {
+//   console.log("ekhon jawar iccha nai");
+// }
+var isGraduated = true;
+var salary = 75000;
+var car = 1;
+if (isGraduated == true && salary > 50000 && car >= 1) {
+  console.log("cholo spain chole jai");
+} else {
+  console.log("ekhon jawar iccha nai");
+}
